10982a9692c0e432eefd15a1de3e5c9ced3075a3
laprasdrum <laprasdrum@yuyacat.com>
2019-03-11 06:39:07 +0900

    fix brew tap

--- pkg/brew/lineemoji_dl.rb
+++ pkg/brew/lineemoji_dl.rb
@@ -5,10 +5,10 @@ class LineEmojiDL < Formula
 
   if OS.mac?
       url "https://github.com/laprasdrum/lineemoji_dl/releases/download/#{version}/lineemoji_dl-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "LineEmojiDL"
+      sha256 "53c643eb8b2ff9fe7ea7056168710d11d4e157c4fc2c06a24249a0590515d6c4"
   elsif OS.linux?
       url "https://github.com/laprasdrum/lineemoji_dl/releases/download/#{version}/lineemoji_dl-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "LineEmojiDL"
+      sha256 "5eb6aff872f4e0934491ef0467fe902bd73e25c580921667e4dab28f5a170db5"
   end
 
   def install
